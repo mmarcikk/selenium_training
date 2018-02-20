a41209cbfdcdd7d86b44582362c7a60118035fdb
mchimiak <marta.chimiak@stxnext.pl>
2018-02-20 15:46:27 +0100

    add test that counts stickers

---
+++ test_counting_stickers.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    # request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_number_of_sticker(driver):
+    driver.get("http://localhost:8084/admin")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("login").click()
+    driver.find_element_by_css_selector('[title=Catalog]').click()
+    sticker =  driver.find_elements_by_css_selector('.sticker')
+    print (len(sticker))
