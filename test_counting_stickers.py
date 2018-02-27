1f4304b7358021aad672856547376197b7401cab
mchimiak <marta.chimiak@stxnext.pl>
2018-02-27 15:56:20 +0100

    fix for sticker counter

--- test_counting_stickers.py
+++ test_counting_stickers.py
@@ -10,10 +10,14 @@ def driver(request):
 
 
 def test_number_of_sticker(driver):
-    driver.get("http://localhost:8084/admin")
-    driver.find_element_by_name("username").send_keys("admin")
-    driver.find_element_by_name("password").send_keys("admin")
-    driver.find_element_by_name("login").click()
-    driver.find_element_by_css_selector('[title=Catalog]').click()
-    sticker =  driver.find_elements_by_css_selector('.sticker')
+    driver.get("http://localhost:8084/")
+    products = driver.find_elements_by_css_selector('li.product')
+    product_counter = len(products) + 1
+    for sticker in range(1, product_counter):
+        sticker =  driver.find_elements_by_css_selector('.sticker')
+        print(sticker)
+        if sticker:
+            print('There is a sticker')
+        else:
+            print('No sticker for that product')
     print (len(sticker))
